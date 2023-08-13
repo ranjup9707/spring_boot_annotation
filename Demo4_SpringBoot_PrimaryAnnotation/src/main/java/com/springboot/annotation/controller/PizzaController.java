7beaf0feccc76cbf2dd622e55a259be2d5c63975
Ranju <ranjupillai2016@gmail.com>
2023-08-13 22:05:26 +0530

    Annotation - Primary

---
+++ Demo4_SpringBoot_PrimaryAnnotation/src/main/java/com/springboot/annotation/controller/PizzaController.java
@@ -0,0 +1,22 @@
+package com.springboot.annotation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import com.springboot.annotation.service.Pizza;
+
+@Component 
+public class PizzaController 
+{
+	private Pizza pizza;
+
+	@Autowired 
+	public PizzaController(Pizza pizza) {
+		this.pizza = pizza;
+	}
+	
+	public String getPizza() {
+		return pizza.getPizza();
+	}
+}
---
+++ Demo4_SpringBoot_PrimaryAnnotation/src/main/java/com/springboot/annotation/service/VegPizza.java
@@ -0,0 +1,14 @@
+package com.springboot.annotation.service;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class VegPizza implements Pizza
+{
+	@Override
+	public String getPizza() {
+		return "Veg Pizza!";
+	}
+}
